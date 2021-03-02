f9b12dd59db029e3610309fe7a8eb283cc3d73de
unknown <sskthree09@gmail.com>
2021-03-02 10:09:10 -0500

    hi

---
+++ src/test/java/Airtel/Tamil/AppTest.java
@@ -0,0 +1,30 @@
+package Airtel.Tamil;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+@Test	
+void test() {
+
+	Assert.assertTrue(true);
+}
+
+@Test	
+void test2() {
+
+	Assert.assertTrue(true);
+}
+
+@Test	
+void test3() {
+
+	Assert.assertTrue(true);
+}
+
+}
